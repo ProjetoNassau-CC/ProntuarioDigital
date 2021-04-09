4fbcc594302457aab641d9e880715148490494ee
wrubenp9 <wesleyruben213@hotmail.com>
2021-04-09 19:08:57 -0300

    Ateração no main, paciente, funcionario, endereco, ficha_tecnica

--- prontuario/src/prontuario/Anamnese.java
+++ prontuario/src/prontuario/Anamnese.java
@@ -1,120 +1,101 @@
 package prontuario;
 
-import java.util.ArrayList;
-
 public class Anamnese {
-	private Boolean problemaAnestesia;
-	private Boolean problemaHemorragia;
-	private Boolean febreReumatica;
-	private Boolean	problemaRenal;
-	private Boolean problemaRespitorio;
-	private Boolean problemaArticularReumatico;
-	private Boolean problemaCardiaco;
-	private Boolean problemaGastrico;
+	private String problemaAnestesia;
+	private String problemaHemorragia;
+	private String febreReumatica;
+	private String problemaRenal;
+	private String problemaRespitorio;
+	private String problemaArticular;
+	private String problemaCardiaco;
+	private String problemaGastrico;
 	private String antecedenteFamiliar;
-	private ArrayList<String> alergia;
+	private String alergia;
 	
-	public Anamnese(Boolean problemaAnestesia, Boolean problemaHemorragia, Boolean febreReumatica,
-			Boolean problemaRenal, Boolean problemaRespitorio, Boolean problemaArticularReumatico,
-			Boolean problemaCardiaco, Boolean problemaGastrico, String antecedenteFamiliar, ArrayList<String> alergia) {
-		this.problemaAnestesia =  problemaAnestesia;
-		this.problemaHemorragia =  problemaHemorragia;
-		this.febreReumatica =  febreReumatica;
-		this.problemaRenal =  problemaRenal;
-		this.problemaRespitorio =  problemaRespitorio;
-		this.problemaArticularReumatico =  problemaArticularReumatico;
-		this.problemaCardiaco =  problemaCardiaco;
-		this.problemaGastrico =  problemaGastrico;
-		this.antecedenteFamiliar =  antecedenteFamiliar;
-		this.alergia =  alergia;
+	public Anamnese(String problemaAnestesia, String problemaHemorragia, String febreReumatica,
+			String problemaRenal, String problemaRespitorio, String problemaArticular,
+			String problemaCardiaco, String problemaGastrico, String antecedenteFamiliar, String alergia) {
+		this.problemaAnestesia = problemaAnestesia;
+		this.problemaHemorragia = problemaHemorragia;
+		this.febreReumatica = febreReumatica;
+		this.problemaRenal = problemaRenal;
+		this.problemaRespitorio = problemaRespitorio;
+		this.problemaArticular = problemaArticular;
+		this.problemaCardiaco = problemaCardiaco;
+		this.problemaGastrico = problemaGastrico;
+		this.antecedenteFamiliar = antecedenteFamiliar;
+		this.alergia = alergia;
 	}
+			
 	
-	
-	public Anamnese(int problemaAnestesia, int problemaHemorragia, int febreReumatica,
-			int problemaRenal, int problemaRespitorio, int problemaArticularReumatico,
-			int problemaCardiaco, int problemaGastrico, String antecedenteFamiliar, ArrayList<String> alergia) {
-		setProblemaAnestesia(problemaAnestesia);
-		setProblemaHemorragia(problemaHemorragia);
-		setFebreReumatica(febreReumatica);
-		setProblemaRenal(problemaRenal);
-		setProblemaRespitorio(problemaRespitorio);
-		setProblemaArticularReumatico(problemaArticularReumatico);
-		setProblemaCardiaco(problemaCardiaco);
-		setProblemaGastrico(problemaGastrico);
-		this.antecedenteFamiliar =  antecedenteFamiliar;
-		this.alergia =  alergia;
+
+	public Anamnese() {
+		
 	}
 
-	public Boolean getProblemaAnestesia() {
+
+	public String getProblemaAnestesia() {
 		return problemaAnestesia;
 	}
 
-	public void setProblemaAnestesia(int problemaAnestesia) {
-		Boolean probAnestesia =  problemaAnestesia ==  0 ? true : false;
-		this.problemaAnestesia =  probAnestesia;
+	public void setProblemaAnestesia(String problemaAnestesia) {
+		this.problemaAnestesia = problemaAnestesia;
 	}
 
-	public Boolean getProblemaHemorragia() {
+	public String getProblemaHemorragia() {
 		return problemaHemorragia;
 	}
 
-	public void setProblemaHemorragia(int problemaHemorragia) {
-		Boolean probHemorragia =  problemaHemorragia ==  0 ? true : false;
-		this.problemaHemorragia =  probHemorragia;
+	public void setProblemaHemorragia(String problemaHemorragia) {
+		this.problemaHemorragia = problemaHemorragia;
 	}
 
-	public Boolean getFebreReumatica() {
+	public String getFebreReumatica() {
 		return febreReumatica;
 	}
 
-	public void setFebreReumatica(int febreReumatica) {
-		Boolean febReumatica =  febreReumatica ==  0 ? true : false;
-		this.febreReumatica =  febReumatica;
+	public void setFebreReumatica(String febreReumatica) {
+		this.febreReumatica = febreReumatica;
 	}
 
-	public Boolean getProblemaRenal() {
+	public String getProblemaRenal() {
 		return problemaRenal;
 	}
 
-	public void setProblemaRenal(int problemaRenal) {
-		Boolean probRenal =  problemaRenal ==  0 ? true : false;
-		this.problemaRenal =  probRenal;
+	public void setProblemaRenal(String problemaRenal) {
+		this.problemaRenal = problemaRenal;
 	}
 
-	public Boolean getProblemaRespitorio() {
+	public String getProblemaRespitorio() {
 		return problemaRespitorio;
 	}
 
-	public void setProblemaRespitorio(int problemaRespitorio) {
-		Boolean probRespitorio =  problemaRespitorio ==  0 ? true : false;
-		this.problemaRespitorio =  probRespitorio;
+	public void setProblemaRespitorio(String problemaRespitorio) {
+		this.problemaRespitorio = problemaRespitorio;
 	}
 
-	public Boolean getProblemaArticularReumatico() {
-		return problemaArticularReumatico;
+	public String getProblemaArticular() {
+		return problemaArticular;
 	}
 
-	public void setProblemaArticularReumatico(int problemaArticularReumatico) {
-		Boolean probArticularReumatico =  problemaArticularReumatico ==  0 ? true : false;
-		this.problemaArticularReumatico =  probArticularReumatico;
+	public void setProblemaArticular(String problemaArticular) {
+		this.problemaArticular = problemaArticular;
 	}
 
-	public Boolean getProblemaCardiaco() {
+	public String getProblemaCardiaco() {
 		return problemaCardiaco;
 	}
 
-	public void setProblemaCardiaco(int problemaCardiaco) {
-		Boolean probCardiaco =  problemaCardiaco ==  0 ? true : false;
-		this.problemaCardiaco =  probCardiaco;
+	public void setProblemaCardiaco(String problemaCardiaco) {
+		this.problemaCardiaco = problemaCardiaco;
 	}
 
-	public Boolean getProblemaGastrico() {
+	public String getProblemaGastrico() {
 		return problemaGastrico;
 	}
 
-	public void setProblemaGastrico(int problemaGastrico) {
-		Boolean probGastrico =  problemaGastrico ==  0 ? true : false;
-		this.problemaGastrico =  probGastrico;
+	public void setProblemaGastrico(String problemaGastrico) {
+		this.problemaGastrico = problemaGastrico;
 	}
 
 	public String getAntecedenteFamiliar() {
@@ -122,36 +103,28 @@ public class Anamnese {
 	}
 
 	public void setAntecedenteFamiliar(String antecedenteFamiliar) {
-		this.antecedenteFamiliar =  antecedenteFamiliar;
+		this.antecedenteFamiliar = antecedenteFamiliar;
 	}
 
 	public String getAlergia() {
-		String alergias = "";
-		if (alergia.size() > 0) {
-			for (String ale : alergia) {
-				alergias += ale + "\n"; 
-			}
-		} else 
-		{
-			alergias = "Não";
-		}
-		return alergias;
+		return alergia;
 	}
 
-	public void setAlergia(ArrayList<String> alergia) {
-		this.alergia =  alergia;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Problema Anestesia? " + problemaAnestesia + "\nProblema Hemorragia? " + problemaHemorragia
-				+ "\nFebre Reumatica? " + febreReumatica + "\nProblema Renal? " + problemaRenal + "\nProblema Respitorio? "
-				+ problemaRespitorio + "\nProblema Articular Reumatico? " + problemaArticularReumatico
-				+ "\nProblema Cardíaco? " + problemaCardiaco + "\nProblema Gástrico? " + problemaGastrico
-				+ "\nAntecedente Familiar? " + antecedenteFamiliar + "\nAlergias? " + getAlergia() + "\n";
+	public void setAlergia(String  alergia) {
+		this.alergia = alergia;
 	}
 	
+	
+	public String toString() {
+		return "\nProblema com Anestesia? " + problemaAnestesia + "\nProblema de Hemorragia? " + problemaHemorragia
+				+ "\nProblema de Febre Reumática? " + febreReumatica + "\nProblema Renal? " + problemaRenal + "\nProblema Respiratório? "
+				+ problemaRespitorio + "\nProblema com Articulação? " + problemaArticular
+				+ "\nProblema Cardíaco? " + problemaCardiaco + "\nProblema Gástrico? " + problemaGastrico
+				+ "\nAntecedentes Familiar?" + antecedenteFamiliar + "\nAlergia? " + alergia;
+	}
+
+
 	
 }
+
 
--- prontuario/src/prontuario/Dentista.java
+++ prontuario/src/prontuario/Dentista.java
@@ -1,22 +1,41 @@
 package prontuario;
 
+import java.util.Scanner;
+
 public class Dentista extends Funcionario {
+	Scanner input = new Scanner(System.in);
 	
 	private String cro;
 
-	public Dentista(int codigo, String nome, String email, String tel, String cel, Endereco endereco,
-			String data_nascimento, String cpf, String rg, String observacao) {
-		super(codigo, nome, email, tel, cel, endereco, data_nascimento, cpf, rg);
+
+	public Dentista(int codigo, String Cro, String nome, String email, String tel, String cel,
+			String data_nascimento, String cpf, String rg,  Endereco endereco) {
+		super(codigo, nome, email, tel, cel, data_nascimento, cpf, rg,  endereco);
 		
 	}
-
+	
+	public Dentista() {
+	
+	}
+	
+	public void questDentista() {
+		
+	}
+	
 	public String getCro() {
 		return cro;
 	}
 
 	public void setCro(String cro) {
 		this.cro = cro;
+			
+
 	}
+	
+	public String toString() {
+		return "\n CRO: "+ getCro() + "\nNome: "+ getNome();
+	}
+	
 
 }
 
